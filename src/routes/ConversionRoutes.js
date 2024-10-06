a491701f282c5d46fa305a8120de2be6890ac274
Caio Marques <caiohenrique1146@gmail.com>
2024-10-06 17:27:58 -0300

    convert

--- src/routes/ConversionRoutes.js
+++ src/routes/ConversionRoutes.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 const ConversionController = require('../controllers/ConversionController')
 const verifyToken = require('../helpers/verify-token')
 
+router.get("/convert", (req, res) => {
+  return res.json({ message: "Conversion"})
+})
 router.post('/convert', verifyToken, ConversionController.convert)
 
 module.exports = router
