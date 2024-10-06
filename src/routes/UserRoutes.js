17f9436695ee8640756c628ca35d5a4e534db6f1
Caio Marques <caiohenrique1146@gmail.com>
2024-10-06 12:39:32 -0300

    att2

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,9 +4,6 @@ const UserController = require('../controllers/UserController')
 //middlewares
 const verifyToken = require('../helpers/verify-token')
 
-router.get('/', (req, res) => {
-  return res.json({ message: "Seja bemnvido á API nexus" })
-})
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
--- src/server.js
+++ src/server.js
@@ -11,8 +11,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
-app.use(express.static('public'))
+app.get("/", (req, res) => {
+  return res.json({ message: "Seja BEMVINDO"})
+})
 
 app.use('/users', userRoutes)
 app.use('/favorites', favoriteRoutes)
